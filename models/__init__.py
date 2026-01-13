a88f4882c52ec2cce8be2c2e75956ca8e577a183
Anonymous <you@example.com>
2026-01-13 09:41:58 +0000

    clean project

--- models/__init__.py
+++ models/__init__.py
@@ -9,7 +9,6 @@ from models.vgg.modular_vgg_model import cifar10_modular_vgg16_bn as ModularVGG1
 from models.resnet.resnet_model import ResNet18
 from models.resnet.modular_resnet_model import modular_resnet18_generator as ModularResNet18
 
-from models.senet.se_resnet_model import SEResNet18
 
 supported_models = ["vgg16", "resnet18", "mobilenet"]
 
@@ -21,8 +20,6 @@ def create_modular_model(model_type, num_classes, modular_training_mode=False):
         model = ResNet18(num_classes=num_classes)
     elif model_type == "mobilenet":
         model = MobileNet(num_classes=num_classes)
-    elif model_type == "senet":
-        model = SEResNet18(num_classes=num_classes)
     elif model_type == "lenet5":
         model = LeNet5(num_classes=num_classes)
     else:
